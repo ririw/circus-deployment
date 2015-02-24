6fe4def83068f2021c4e7b2433acf171b87ae183
Richard Weiss <richardweiss@richardweiss.org>
2015-02-24 23:06:18 +1100

    altered run script

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 service nginx start
 
+container_address=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+host_address=$(/sbin/ip route|awk '/default/ { print $3 }')
+echo $internet_address
+
+curl -L -X PUT http://$host_address:4001/v2/keys/circusoc -d value="$container_address"
+
+
 cd /root
 java -jar /root/backend.jar
 #python -m SimpleHTTPServer
